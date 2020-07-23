f6138b0a2169893065ef23ec8577bb8f1ed0f26c
aliaslazaris <aliaslazaris@gmail.com>
2020-07-22 19:18:15 -0500

    added param

--- jenkinsBuild.groovy
+++ jenkinsBuild.groovy
@@ -35,21 +35,19 @@ def slavePodTemplate = """
             hostPath:
               path: /var/run/docker.sock
     """
-    def environment = ""
+    def environment  = ""
     def docker_image = ""
-    def branch          = "${scm.branches[0].name}".replaceAll(/^\*\//, '').replace("/", "-").toLowerCase()
-
+    def branch = "${scm.branches[0].name}".replaceAll(/^\*\//, '').replace("/", "-").toLowerCase()
     docker_image = "aida77/artemis:${branch.replace('version/', 'v')}"
-
+    // master -> prod  dev-feature/* -> dev qa-feature/* -> qa 
     if (branch == "master") {
       environment = "prod"
-    } else if (branch.contains('dev-feature')){
+    } else if (branch.contains('dev-feature')) {
       environment = "dev"
-    } else if (branch.contains('qa-feature')){
+    } else if (branch.contains('qa-feature')) {
       environment = "qa"
     }
     println("${environment}")
-
     podTemplate(name: k8slabel, label: k8slabel, yaml: slavePodTemplate, showRawYaml: false) {
       node(k8slabel) {
         stage('Pull SCM') {
@@ -58,30 +56,28 @@ def slavePodTemplate = """
         container("docker") {
             dir('deployments/docker') {
                 stage("Docker Build") {
-                    sh "docker build -t ${docker_image} ."
+                  sh "docker build -t ${docker_image}  ."
                 }
                 stage("Docker Login") {
                     withCredentials([usernamePassword(credentialsId: 'docker-hub-creds', passwordVariable: 'password', usernameVariable: 'username')]) {
-                        sh "docker login --username ${username} --password ${password}"
+                      sh "docker login --username ${username} --password ${password}"
                     }
                 }
                 stage("Docker Push") {
-                    sh "docker push ${docker_image}"
+                  sh "docker push ${docker_image}"
                 }
-
                 stage("Trigger Deploy") {
                   build job: 'artemis-deploy', 
                   parameters: [
-                      [$class: 'BooleanParameterValue', name: 'terraformApply',     value: true],
-                      [$class: 'StringParameterValue',  name: 'environment',        value: "${environment}"]
-                      [$class: 'StringParameterValue',  name: 'docker_image',       value: "${docker_image}"]
+                      [$class: 'BooleanParameterValue', name: 'terraformApply', value: true],
+                      [$class: 'StringParameterValue',  name: 'environment', value: "${environment}"],
+                      [$class: 'StringParameterValue',  name: 'docker_image', value: "${docker_image}"]
                       ]
                 }
             }
         }
       }
     }
-
 
 
 
